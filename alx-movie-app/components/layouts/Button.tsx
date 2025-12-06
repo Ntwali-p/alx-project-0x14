11e1db31db563bc35538bc046921856456cacd3d
Ntwali-p <ntwali.pacific@gmail.com>
2025-12-06 11:19:29 +0200

    changes

--- alx-movie-app/components/layouts/Button.tsx
+++ alx-movie-app/components/layouts/Button.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <button className="px-3 py-2 bg-green-600 text-white rounded">
-      Layout Button
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
     </button>
   );
 };
--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,22 +1,17 @@
-import React, { ReactNode } from "react";
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-const Layout = ({ children }: LayoutProps) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Header />
-      <main className="flex-1 container mx-auto p-4">
-        {children}
-      </main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 
 export default Layout;
+
 
